a58748ec6e04d2c57c17a12dc3bd8312093389df
Andre Santiago <the.asantiagojr@gmail.com>
2018-06-06 19:14:30 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,7 +3,7 @@
 def position_taken?(board, index)
   if board[index] == " " || "" || nil
     return false
-  else board[index] == "X" || board[index] == "O"
+  elsif board[index] == "X" || board[index] == "O"
     return true
   end
 end
